e0c41535ab73bf3c87b8797091d36c8e02c1faa4
pusapati sathvik <pusapatisathvik@gmail.com>
2024-08-23 14:04:22 +0530

    Update and rename static_methods_return_type._parameters.java to static_methode.java

---
+++ METHODS/static_methode.java
@@ -0,0 +1,19 @@
+class Static_methode
+{
+static void addition()
+  {
+     int a=10,b=10,c=a+b;
+     System.out.println(c);
+
+    
+  }
+
+  public static void main(String[] arg)
+  {
+
+       addition();
+
+    
+  }
+
+}
