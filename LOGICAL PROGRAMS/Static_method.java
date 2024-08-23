cb5fbb9b918a3fdb235abb0b44232e225323afbb
pusapati sathvik <pusapatisathvik@gmail.com>
2024-08-24 00:24:29 +0530

    Add files via upload

---
+++ LOGICAL PROGRAMS/Static_method.java
@@ -0,0 +1,19 @@
+class Method1{
+  
+    static void add()
+  {
+     int a=10,b=10,c=a+b;
+     System.out.println("addition is "+c);
+  
+  }
+  
+  }
+     public class Static_method {
+    public static void main(String[] args)
+    {   
+        Method1.add();
+
+
+    }
+    
+}
