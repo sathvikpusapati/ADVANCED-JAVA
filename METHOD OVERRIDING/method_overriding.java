a46692dc0a228e3c5a6aa02331d190ee299f16f1
pusapati sathvik <pusapatisathvik@gmail.com>
2024-09-01 14:02:09 +0530

    Update method_overriding.java

--- METHOD OVERRIDING/method_overriding.java
+++ METHOD OVERRIDING/method_overriding.java
@@ -21,17 +21,23 @@ class method_overriding
         
         //i am using inheritence so i can call the parent class method from child class
         // but we are doing method over riding so i will get the method which was in sub class
-        bike_200cc obj=new bike_200cc();
+        bike_150cc obj=new bike_200cc(); //upcasting
 
+        System.out.println("now i am using upcasting \n");
         obj.speed();
-
-        bike_150cc obj2=new bike_150cc();
+        
+        System.out.println("\n now i am using method overriding in the same class\n");
+        bike_150cc obj2=new bike_150cc();//over riding
         obj2.speed();
 
+        	
+        System.out.println("\nagain using upcasting\n");
+        
         bike_150cc obj3=new bike_200cc();//sub class object is referenced by parentb class
 
         obj3.speed();
 
+       
     }
 
 }
